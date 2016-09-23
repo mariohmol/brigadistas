a5b5cd1fb23815b3f88e83fb8a70528856c6a36c
Mario Mol <mariohmol@gmail.com>
2016-09-23 13:01:30 -0300

    gulp process with es6

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-//require('es6-promise').polyfill()
+require('es6-promise').polyfill()
 var camelCase = require('lodash.camelcase');
 var upperFirst = require('lodash.upperfirst');
 var webpack = require('webpack');
