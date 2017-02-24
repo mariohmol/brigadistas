beaf3810ae79674be3e3f2d7586707b0b7bdcaa5
Mario Mol <mariohmol@gmail.com>
2017-02-24 00:01:00 -0300

    flow navigations with menu options to back and ok on crud

--- brigadistacivil/src/pages/brigade/brigade.ts
+++ brigadistacivil/src/pages/brigade/brigade.ts
@@ -1,26 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {TranslateService} from 'ng2-translate';
-/*
-  Generated class for the Brigade page.
+import BasePage from '../basepage';
+import BrigadesPage from './brigades'
+import { BrigadeService} from '../../providers/brigade-service';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-brigade',
   templateUrl: 'brigade.html'
 })
-export class BrigadePage {
+export class BrigadePage  extends BasePage{
+  public brigade: any;
+  public readonly: boolean;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public translate: TranslateService) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public brigadeService: BrigadeService) {
+    super();
+
+    if(this.navParams.get("brigade")){
+      this.brigade=this.navParams.get("brigade");
+      this.readonly=true;
+    } else{
+      this.brigade={};
+      this.readonly=false;
+    }
+  }
+
+  save(){
+    this.brigadeService.addBrigade(this.brigade).then(d=>{
+      this.openPage(BrigadesPage);
+    });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad BrigadePage');
   }
 
   isReadonly(){
-    return true;
+    return this.readonly;
   }
 
 }
--- brigadistacivil/src/pages/brigade/brigades.ts
+++ brigadistacivil/src/pages/brigade/brigades.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {TranslateService} from 'ng2-translate';
 import {BrigadePage} from './brigade';
-import { BrigadeService} from '../../providers/brigade-service'
+import { BrigadeService} from '../../providers/brigade-service';
+import BasePage from '../basepage';
 
 /*
   Generated class for the Brigade page.
@@ -14,17 +15,20 @@ import { BrigadeService} from '../../providers/brigade-service'
   selector: 'page-brigades',
   templateUrl: 'brigades.html'
 })
-export class BrigadesPage {
-  public brigades: any;
+export class BrigadesPage extends BasePage{
+  public brigades: Array<any>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public translate: TranslateService,
               public brigadeService: BrigadeService) {
+                super();
                 this.brigades=[];
               }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad BrigadesPage');
-    this.brigades = this.brigadeService.getBrigades();
+    this.brigadeService.getBrigades().then(d=>{
+      this.brigades = <Array<any>>d;
+    });
   }
 
   addBrigade(){
