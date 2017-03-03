2563a53f895c82e0ee848f50ad70b0596c09842f
Mario Mol <mariohmol@gmail.com>
2017-03-02 23:36:19 -0300

    Fix assets in mobile

--- brigadistacivil/src/app/app.module.ts
+++ brigadistacivil/src/app/app.module.ts
@@ -33,7 +33,7 @@ let pages = [
     IonicModule.forRoot(MyApp),
     TranslateModule.forRoot({
         provide: TranslateLoader,
-        useFactory: (http: Http) => new TranslateStaticLoader(http, '/assets/i18n', '.json'),
+        useFactory: (http: Http) => new TranslateStaticLoader(http, 'assets/i18n', '.json'),
         deps: [Http]
     })
   ],
