b8650e6b701af86f2bcf9a2c83707e872169acc3
Mario Mol <mariohmol@gmail.com>
2017-03-18 00:02:47 -0300

    Starting brigade server test

--- test/server/_index.js
+++ test/server/_index.js
@@ -4,36 +4,34 @@ global.DATABASE_URL = DATABASE_URL;
 global.PORT = PORT;
 global.ENV = ENV;
 const chai = require('chai');
-const chaiHttp = require('chai-http');
 const passport = require("passport");
-const faker = require('faker');
 const mongoose = require('mongoose');
-const server = require('../../server/server.js');
+const server = require('../../server/server');
+const { User } = require('../../server/user/models');
+const { user,adminUser } =require("../fixtures/user");
 
 const expect = chai.expect;
 const should = chai.should();
 const app = server.app;
 
-chai.use(chaiHttp);
 
 describe('Index page', () => {
 
     before(function (done) {
         mongoose.connect(DATABASE_URL, function(err, dbc){
             mongoose.connection.db.dropDatabase(function(){
+              User.create(user).then(e=>{
+                return User.create(adminUser);
+              }).then(e=>{
                 done();
+              }).catch(e=>{
+                done();
+              });
             });
         });
     });
 
-    it('exists', done => {
-        chai.request(app)
-            .get('/')
-            .end((err, res) => {
-                res.should.have.status(200);
-                //alternative to res.should.be.html
-                expect(res).to.have.header('content-type', 'text/html; charset=UTF-8');
-                done();
-            });
+    it('empty the database for testing', done => {
+        done();
     });
 });
